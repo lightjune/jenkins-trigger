f8c59fd2ef455ef61cae7fd9e209b7e35dec8fed
lightjune <firclip@gmail.com>
2018-10-21 00:47:02 +0900

    [I-1]logging 변경 https://github.com/lightjune/jenkins-trigger/issues/1 logger 설정 변경.

---
+++ init.py
@@ -0,0 +1,9 @@
+import json
+import logging.config
+
+with open('configs/logging.json', 'rt') as f:
+    logging_config = json.load(f)
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger("logger")
+payload_logger = logging.getLogger("payload_logger")
