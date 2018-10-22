7cf9f844b42fb6babc2eebe0de65e652f6a79890
lightjune <firclip@gmail.com>
2018-10-22 23:09:19 +0900

    [I-3]push 이벤트 구분 https://github.com/lightjune/jenkins-trigger/issues/3 event 를 구분하고 처리하기 위한 webhook 모듈 작성.

--- main.py
+++ main.py
@@ -2,13 +2,28 @@ from flask import Flask, render_template, request
 from init import logger, payload_logger
 import json
 
+from webhook import Event
+
+
 application = Flask(__name__)
 
 
 @application.route("/jenkins-trigger", methods=["POST"])
 def jenkins_trigger():
-    logger.info("(%s) %s", request.method, request.path)
+    logger.info(f"({request.method}) {request.path}")
     payload_logger.info(json.dumps(request.form, indent=2))
+
+    event_type = request.headers.get("X-GitHub-Event")
+
+    if not event_type:
+        logger.warn("There is no X-GitHub-Event header")
+        return render_template("empty.html"), 400
+    else:
+        event = Event.factory(event_type, request.form)
+
+    if event and event.is_triggering_event():
+        pass
+
     return render_template("empty.html"), 200
 
 
